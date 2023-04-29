04bd9b1c3bd0115118464fdc77dba113fa288ec6
Brayan <turnerbrayan@gmail.com>
2023-04-28 20:59:46 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- Desktop/NucampFolder/WebDev/3-React/nucampsite/src/pages/CampsitesDirectoryPage.js
+++ Desktop/NucampFolder/WebDev/3-React/nucampsite/src/pages/CampsitesDirectoryPage.js
@@ -4,10 +4,18 @@ import CampsitesList from '../features/campsites/CampsitesList';
 import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
 
 const CampsitesDirectoryPage = () => {
-    const selectedCampsite = selectRandomCampsite();
+    let selectedCampsite = selectRandomCampsite();
+
+    const toggleCampsite = () => {
+        selectedCampsite = selectRandomCampsite();
+        console.log(selectedCampsite);
+    };
 
     return (
         <Container>
+            <Button onClick={() => toggleCampsite()}>
+                Select Random Campsite
+            </Button>
             <Row>
                 <Col sm='5' md='7'>
                     <CampsitesList />
