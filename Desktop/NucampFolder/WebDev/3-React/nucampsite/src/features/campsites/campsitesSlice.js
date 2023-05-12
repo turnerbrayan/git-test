0e333430457bbab9053357164d95b7b751601419
Brayan <turnerbrayan@gmail.com>
2023-05-11 21:39:59 -0500

    Redux campsitesSlice

--- Desktop/NucampFolder/WebDev/3-React/nucampsite/src/features/campsites/campsitesSlice.js
+++ Desktop/NucampFolder/WebDev/3-React/nucampsite/src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { createSlice } from '@reduxjs/toolkit';
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
@@ -11,7 +23,3 @@ export const selectCampsiteById = (id) => {
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
 };
-
-// export const selectRandomCampsite = () => {
-//     return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
-// };
